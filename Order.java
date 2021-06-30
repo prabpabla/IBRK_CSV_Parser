dbea1e0b89a5279953b202e5257cb654c6d6964d
prabpabla <pabla.prab@gmail.com>
2021-06-29 23:06:08 -0400

    Add files via upload

--- Order.java
+++ Order.java
@@ -1,36 +1,57 @@
 
+import java.sql.Time;
+import java.util.Date;
 public class Order {
-
+	
+	private String header;
+	private String dataDiscriminator;
 	private String symbol;
-	private String time;
+	private Time time;
     private int quantity;
-    private float price;
+    private float tPrice;
+    private float proceeds;
     private float realizedPNL;
     private float commissions;
     
-    public Order(String symbol, String time, int quantity, float price, float realizedPNL, float commissions) {
+    
+    public Order(String header, String dataDiscriminator, String symbol, Time time, int quantity, float tPrice, float proceeds, float realizedPNL, float commissions) {
+    	this.header = header;
+    	this.dataDiscriminator = dataDiscriminator;
     	this.symbol = symbol;
         this.time = time;
         this.quantity = quantity;
-        this.price = price;
+        this.tPrice = tPrice;
+        this.proceeds = proceeds;
         this.realizedPNL = realizedPNL;
         this.commissions = commissions;
+    }
+    
+    public String getHeader() {
+    	return this.header;
+    }
+    
+    public String getDataDiscriminator() {
+    	return this.dataDiscriminator;
     }
     
     public String getSymbol() {
     	return this.symbol;
     }
     
-    public String getTime() {
+    public Time getTime() {
     	return this.time;
     }
     
-    public int getQuantityl() {
+    public int getQuantity() {
     	return this.quantity;
     }
     
     public float getPrice() {
-    	return this.price;
+    	return this.tPrice;
+    }
+    
+    public float getProceeds() {
+    	return this.proceeds;
     }
     
     public float getRealizedPNL() {
@@ -42,6 +63,6 @@ public class Order {
     }
     
     public String toString() {
-    	return symbol+" "+time+" "+quantity+" "+price+" "+realizedPNL+" "+commissions;
+    	return header+" "+dataDiscriminator+" "+symbol+" "+time+" "+quantity+" "+tPrice+" "+proceeds+" "+realizedPNL+" "+commissions;
     }
 }
--- TradeLog_Parser.java
+++ TradeLog_Parser.java
@@ -4,17 +4,39 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.Map.Entry;
-
 import javax.lang.model.element.Element;
-
 import org.apache.commons.csv.*;
 import com.opencsv.CSVWriter;
-
-
+import java.sql.Time;
+import java.util.Date;
 public class TradeLog_Parser {
 
 	//Creating a HashMap of Sting to List of Type Order
 	HashMap<String, List<Order>> symbolToOrderList; //null
+	
+	String header = "Dats";
+	String dataDiscriminator = "Order";
+	String symbol = "Symbol";
+	String position = "Long/Short";
+	@SuppressWarnings("deprecation")
+	Time time = new Time(0, 0, 0);
+	@SuppressWarnings("deprecation")
+	Time timeOpen = new Time(0, 0, 0);
+	@SuppressWarnings("deprecation")
+	Time timeClose = new Time(0, 0, 0);
+	Time timeHold = new Time(0, 0, 0);
+	int quantity = 0;
+	float proceeds = 0;
+	float price = 0;
+	float priceOpen = 0;
+	float priceClose = 10000;
+	float commissions = 0;
+	float capital = 0;
+	float realizedPNL = 0;
+	int partials = 0;
+	float results = 0;
+	String scalps = "N";
+	int totalQuantity = 0;
 	
 	public TradeLog_Parser(String filePath) throws IOException{
 		File csvDataFile = new File(filePath);
@@ -23,17 +45,20 @@ public class TradeLog_Parser {
 		System.out.println(parser.getHeaderNames());
 		this.symbolToOrderList = new HashMap<>(); //{}
 
+		/*
 		for (CSVRecord csvRecord : parser) {
 			if(csvRecord.get("DataDiscriminator").equals("Order")) {
+				header = csvRecord.get("Header");
+				dataDiscriminator = csvRecord.get("DataDiscriminator");
+				symbol = csvRecord.get("Symbol");
+				time = Time.valueOf(csvRecord.get("Date/Time"));
+				quantity = Integer.parseInt(csvRecord.get("Quantity"));
+				price = Float.parseFloat(csvRecord.get("T. Price"));
+				proceeds = Float.parseFloat(csvRecord.get("Proceeds"));
+				realizedPNL = Float.parseFloat(csvRecord.get("Realized P/L"));
+				commissions = Float.parseFloat(csvRecord.get("Comm/Fee"));
 				
-				String symbol = csvRecord.get("Symbol");
-				String time = csvRecord.get("Date/Time");
-				int quantity = Integer.parseInt(csvRecord.get("Quantity"));
-				float price = Float.parseFloat(csvRecord.get("T. Price"));
-				float realizedPNL = Float.parseFloat(csvRecord.get("Realized P/L"));
-				float commissions = Float.parseFloat(csvRecord.get("Comm/Fee"));
-				
-				Order order = new Order(symbol, time, quantity, price, realizedPNL, commissions);
+				Order order = new Order(header, dataDiscriminator, symbol, time, quantity, price, proceeds, realizedPNL, commissions);
 
 				//Check is symbol exits in the map, Initialization if statement
 				if(!symbolToOrderList.containsKey(symbol)) { //symbol not in symbolToOrderList
@@ -44,10 +69,32 @@ public class TradeLog_Parser {
 				orderList.add(order); //list.append(order)		
 			} 
 		}
+		*/
+		for (CSVRecord csvRecord : parser) {
+				header = csvRecord.get("Header");
+				dataDiscriminator = csvRecord.get("DataDiscriminator");
+				symbol = csvRecord.get("Symbol");
+				time = dataDiscriminator.equals("Order") ? Time.valueOf(csvRecord.get("Date/Time")) : new Time(0, 0, 0);
+				price = dataDiscriminator.equals("Order") ? Float.parseFloat(csvRecord.get("T. Price")) : 0;
+				quantity = dataDiscriminator.equals("Order") ? Integer.parseInt(csvRecord.get("Quantity")) : 0;
+				proceeds = Float.parseFloat(csvRecord.get("Proceeds"));
+				realizedPNL = Float.parseFloat(csvRecord.get("Realized P/L"));
+				commissions = Float.parseFloat(csvRecord.get("Comm/Fee"));
+				
+				Order order = new Order(header, dataDiscriminator, symbol, time, quantity, price, proceeds, realizedPNL, commissions);
+
+				//Check is symbol exits in the map, Initialization if statement
+				if(!symbolToOrderList.containsKey(symbol)) { //symbol not in symbolToOrderList
+					List<Order> emptyList = new ArrayList<>(); //[]
+					symbolToOrderList.put(symbol, emptyList); //symbolToOrderList[symbol] = []
+				}
+				List<Order> orderList = symbolToOrderList.get(symbol); //list = symbolToOrderList[symbol] 
+				orderList.add(order); //list.append(order)		
+		}
 		
-		//debugOrderMap();
+		debugOrderMap();
 		//debugOrderMap2();
-		System.out.println(totalTrades());
+		//System.out.println(totalTrades());
 		formatOrderToCSV("C:\\Users\\prab_\\Downloads\\Output_20210616.csv");
 		/*symbolToOrderList = {AAPL: [{symbol:AAPL, price:1, quantity:100},{symbol:AAPL, price:1, quantity:200}], 
 								CCL: [{symbol:CCL, price:1, quantity:100}]}
@@ -79,20 +126,7 @@ public class TradeLog_Parser {
 		//If shares != 0, take that Open T. Price, and Time Open
 		//If total shares != 0, update Open T. Price, and Close T. Price
 		//If total shares == 0, update Close T. Price, and Time Close, 
-		int totalQuantity = 0;
-		String symbol = "Symbol";
-		String position = "Long/Short";
-		String timeOpen = "00:00";
-		String timeClose = "99:99";
-		String timeHold = "00:00";
-		int shares = 1234;
-		float priceOpen = 0;
-		float priceClose = 10000;
-		float commissions = 0;
-		float capital = 0;
-		float partials = 0;
-		float results = 0;
-		String scalps = "N";
+		
 		
 		File file = new File(savePath);
 		FileWriter output = new FileWriter(file);
@@ -106,27 +140,35 @@ public class TradeLog_Parser {
 					if(totalQuantity==0) {
 						
 						symbol = orderEntry.getSymbol();
-						position = totalQuantity > 0 ? "Long" : "Short";
-						timeOpen = orderEntry.getTime();
-						shares = orderEntry.getQuantity();
-						priceOpen = orderEntry.getPrice();
+						
+						timeClose = orderEntry.getTime();
+						
+						priceClose = orderEntry.getPrice();
 						commissions = orderEntry.getCommissions();
-						capital = shares*priceOpen;
+						capital = quantity*priceOpen;
 						partials++;
+						//timeHold = timeOpen - timeClose;
 						scalps = partials > 1? "Y" : "N";
+						String[] trade = {symbol, position, String.valueOf(priceOpen), String.valueOf(priceClose), String.valueOf(quantity), 
+								timeOpen.toString(), timeClose.toString(), timeHold.toString(), String.valueOf(capital), String.valueOf(results), String.valueOf(partials), scalps};
+						System.out.println(Arrays.toString(trade));
+						
 					}
 					else {
+						position = totalQuantity > 0 ? "Long" : "Short";
 						symbol = orderEntry.getSymbol();
-						timeClose = orderEntry.getTime();
+						timeOpen = orderEntry.getTime();
 						//timeHold = String.valueOf(Float.parseFloat(timeOpen)-Float.parseFloat(timeClose));
-						priceClose = orderEntry.getPrice();
+						priceOpen = orderEntry.getPrice();
+						quantity = orderEntry.getQuantity();
 						commissions += orderEntry.getCommissions();
 						results = orderEntry.getRealizedPNL();
-						String[] trade = {symbol, position, String.valueOf(priceOpen), String.valueOf(priceClose), String.valueOf(shares), 
-											timeOpen, timeClose, timeHold, String.valueOf(capital), String.valueOf(results), String.valueOf(partials), scalps};
+						String[] trade = {symbol, position, String.valueOf(priceOpen), String.valueOf(priceClose), String.valueOf(quantity), 
+											timeOpen.toString(), timeClose.toString(), timeHold.toString(), String.valueOf(capital), String.valueOf(results), String.valueOf(partials), scalps};
 						write.writeNext(trade, false);
 						commissions = 0;
 						partials = 0;
+						//System.out.println(Arrays.toString(trade));
 					}
 				}
 			}
